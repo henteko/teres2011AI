90b2731e71d7bfe4081d94be9d2e03a440687552
hota <magurotoebigasuki@gmail.com>
2011-07-28 02:14:07 +0900

    shuffle_card desu

--- 2011AI/2011AI/shuffle_card.cpp
+++ 2011AI/2011AI/shuffle_card.cpp
@@ -1,1 +1,34 @@
-shuffle card
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+ 
+  
+ 
+ void shuffle_card(void)
+ {
+	 int n[52];
+	srand( (unsigned)time( NULL ) );
+	
+	  for(int j=0; j<52;j++)
+	  {
+	    n[j]=j;
+	  }
+	 
+	 
+	 int ran=52;
+	 int temp;
+	 
+	 for(int j=52; j>0;j--)
+	 {
+	 int p=j-1;
+	 int t=rand()%ran;
+	     temp = n[p];
+	     n[p]= n[t];
+		 n[t]=temp;
+		 ran=ran-1;
+	 }
+
+	 
+	 return;
+}
+
