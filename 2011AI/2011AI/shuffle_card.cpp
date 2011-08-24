1601fea823931d8bd64ed05edde89a23d394b4f5
hota <magurotoebigasuki@gmail.com>
2011-08-24 13:09:27 +0900

    finish?

--- 2011AI/2011AI/shuffle_card.cpp
+++ 2011AI/2011AI/shuffle_card.cpp
@@ -1,24 +1,37 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
- 
-  
- 
- void shuffle_card(void)
+
+
+/*
+説明
+returnだと配列は返せないので
+int mainのほうで
+int shuffle_card_result[CARD_NUM_MAX]
+と宣言しています。
+そしてshuffle_card関数の引数をshuffle_card_resultとすることで
+擬似的に配列を返す仕様になっています。
+。
+
+さらに配列の大きさ指定には変数は使えないので
+#define CARD_NUM_MAX 52
+と宣言してようやっと動くようになっています。
+
+*/
+ void shuffle_card(int shuffle_card_result[])
  {
-	 int n[52];
-	srand( (unsigned)time( NULL ) );
-	
-	  for(int j=0; j<52;j++)
+	   
+     int n[CARD_NUM_MAX];
+	 srand( (unsigned)time( NULL ) );
+	  int NUMcard = 1;
+	  for(int j=0; j<CARD_NUM_MAX;j++)
 	  {
-	    n[j]=j;
+	    n[j]=NUMcard;
+		NUMcard++;
 	  }
 	 
 	 
-	 int ran=52;
+	 int ran=CARD_NUM_MAX;
 	 int temp;
 	 
-	 for(int j=52; j>0;j--)
+	 for(int j=CARD_NUM_MAX; j>0;j--)
 	 {
 	 int p=j-1;
 	 int t=rand()%ran;
@@ -27,8 +40,10 @@
 		 n[t]=temp;
 		 ran=ran-1;
 	 }
-
 	 
-	 return;
+	 for(int j=0; j<CARD_NUM_MAX;j++)
+	  {
+	 shuffle_card_result[j]=n[j];
+	 }
+	
 }
-
