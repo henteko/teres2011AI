4c2f23c8fbb8caddbf1d90bad8904ee26cc50ae0
kemukemu27 <kemukemu27@yahoo.co.jp>
2011-08-31 22:03:19 +0900

    Judge_Num kousin2

--- 2011AI/2011AI/Judge_Num.cpp
+++ 2011AI/2011AI/Judge_Num.cpp
@@ -5,9 +5,9 @@ int judge_Num(int return_start_Ai)
 int r = return_start_Ai;
 for(int i=0;i<CARD_MARK;i++)
 {
-	for(int j=1;j<=CARD_NUM_MAX;j++)
+	for(int j=i+1;j<CARD_NUM_MAX;j++)
 	{
-		if(r == ((CARD_NUM_MAX*i)+j+1))
+		if(r == ((CARD_NUM_MAX*i)+j))
 		{
 			if(r < ((CARD_NUM_MAX*j)-((CARD_NUM_MAX-1)/2)))
 			{
